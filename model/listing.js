0b58910d9a09839f608bfde309d3ccf5f352d276
mehwishtahseen <161937725+mehwishtahseen@users.noreply.github.com>
2024-08-27 00:00:41 +0530

    updated new.ejs

--- model/listing.js
+++ model/listing.js
@@ -1,29 +1,3 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const listingSchema = new Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: String,
-  
-//   image: {
-//     filename: String,
-//     url: {
-//       type: String,
-//       default: "https://images.pexels.com/photos/17644421/pexels-photo-17644421/free-photo-of-seagulls-flying-on-sea-shore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-//       set: (v) => v === "" ? "https://images.pexels.com/photos/17644421/pexels-photo-17644421/free-photo-of-seagulls-flying-on-sea-shore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" : v,
-//     }
-//   },
-
-//   price: Number,
-//   location: String,
-//   country: String,
-// });
-
-// const Listing = mongoose.model("Listing", listingSchema);
-// module.exports = Listing;
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
