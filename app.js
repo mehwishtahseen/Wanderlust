a4842bd70a993a2f66b54ddc2ed30da3f64c32c8
mehwishtahseen <161937725+mehwishtahseen@users.noreply.github.com>
2024-08-27 08:58:25 +0530

    updated

--- app.js
+++ app.js
@@ -80,25 +80,8 @@ if(typeof req.file !== "undefined"){
       listing.image = {url, filename};
       await listing.save();
     }
-    res.redirect(`listings/$ {id}`);
+    res.redirect(`/listings/ ${id}`);
 });
-
-// module.exports.updateListing = async(req,res)=>{
-//     let {id} = req.params;
-
-//     let listing =  await Listing.findByIdAndUpdate(id , {...req.body.listing});
-
-//     if(typeof req.file!=="undefined"){
-//         let url =req.file.path;
-//         let filename = req.file.filename;
-//         listing.image = {url , filename};
-//         await listing.save();
-//     }
-
-//     req.flash("success" , " Listing Updated Successfuly !")
-
-//     res.redirect(`/listings/${id}`);
-// };
 
 //Delete Route
 
